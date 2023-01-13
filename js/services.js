f7a424166572bfe21143d13b470cf202973900f5
Ejaz-100400 <ejazahmed4688@gmail.com>
2023-01-13 12:14:27 +0530

    services cont 13/1

--- js/services.js
+++ js/services.js
@@ -24,3 +24,66 @@ window.onscroll=function(){
         document.querySelector('.resp-header').style.backgroundColor = 'transparent';
     }
 }
+
+
+// SERVICES SECTION 
+
+// closing services content
+const closeicon=document.querySelectorAll('.ri-close-line')
+closeicon.forEach((close)=>{
+    close.addEventListener('click', function(e){
+        if(e.target){
+        $('.services-content').fadeToggle(400);
+        }
+    })
+}) 
+const serviceitem1 = document.querySelectorAll('.services-item')[0]
+serviceitem1.addEventListener('click',function(e){
+        if(e.target){
+            $('#headclean').fadeIn(100);
+            $('.services-content').fadeToggle(400);
+        }
+    })
+const serviceitem2 = document.querySelectorAll('.services-item')[1]
+serviceitem2.addEventListener('click',function(e){
+    if(e.target){
+         $('#projfit').fadeIn(100);
+        $('.services-content').fadeToggle(400);
+    }
+})
+const serviceitem3 = document.querySelectorAll('.services-item')[2]
+serviceitem3.addEventListener('click',function(e){
+    if(e.target){
+         $('#foglamp').fadeIn(100);
+        $('.services-content').fadeToggle(400);
+    }
+})
+const serviceitem4 = document.querySelectorAll('.services-item')[3]
+serviceitem4.addEventListener('click',function(e){
+    if(e.target){
+         $('#sysspeak').fadeIn(100);
+        $('.services-content').fadeToggle(400);
+    }
+})
+const serviceitem5 = document.querySelectorAll('.services-item')[4]
+serviceitem5.addEventListener('click',function(e){
+    if(e.target){
+         $('#barlight').fadeIn(100);
+        $('.services-content').fadeToggle(400);
+    }
+})
+const serviceitem6 = document.querySelectorAll('.services-item')[5]
+serviceitem6.addEventListener('click',function(e){
+    if(e.target){
+         $('#headwiring').fadeIn(100);
+        $('.services-content').fadeToggle(400);
+    }
+})
+const serviceitem7 = document.querySelectorAll('.services-item')[6]
+serviceitem7.addEventListener('click',function(e){
+    if(e.target){
+         $('#drl').fadeIn(100);
+        $('.services-content').fadeToggle(400);
+    }
+})
+    
